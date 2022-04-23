6fa77a07f51b65971552d195489769bca3a01480
Mag10955 <magdalena.doerfler@isc.fraunhofer.de>
2022-04-23 16:27:19 +0200

    added values for camera sensitivity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
-
+import numpy as np
+import matplotlib.pyplot as plt
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
