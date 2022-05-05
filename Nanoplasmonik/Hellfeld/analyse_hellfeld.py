4334cbdd9da3123cb363b8912659f40acdb9acbb
Neo-practise <marius.kaiser.mail@web.de>
2022-05-05 15:06:05 +0200

    Korrekturen und Berechnung des FWHM

--- Nanoplasmonik/Hellfeld/analyse_hellfeld.py
+++ Nanoplasmonik/Hellfeld/analyse_hellfeld.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy.fft import fft, ifft
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy.special import voigt_profile as voigt
 
 # werte = np.loadtxt("dips4.txt")
 # x, y, b, nichts, c, d = np.hsplit(werte, 6)
@@ -20,6 +21,31 @@ def gaussian(x, mu, sig, fac):
 
 def faltung( x, w0, gamma, mu, sig, fac ):
     return lorentz(x, w0, gamma)*gaussian(x, mu, sig, fac)
+
+
+
+def f_L(w0, gamma):
+    return np.sqrt( w0**2+gamma*w0 ) - np.sqrt( w0**2-gamma*w0 )
+def f_G(sigma):
+    return 2 * np.sqrt(2*np.log(2)) * sigma
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+def FWHM( x, y ): # Gibt Float-Array zurück [ fwhm, index links, index rechts ]
+    ID = np.zeros(2, dtype = int)
+    half_values = np.zeros(2, dtype=float)
+    ID_max = list(y).index(y.max())
+    half_values[0] = find_nearest(y[0:ID_max-1],y.max()/2)
+    half_values[1] = find_nearest(y[ID_max + 1:-1], y.max() / 2)
+    ID[0] = list(y).index(half_values[0])
+    ID[1] = list(y).index(half_values[1])
+    fwhm = np.array( [np.abs(w[ID[0]]-w[ID[1]]), ID[0], ID[1] ], dtype = float )
+    #print(ID)
+    return fwhm
+
 
 #def gaussian(x, mu, sig, fac):
 #    return fac * np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
@@ -45,7 +71,7 @@ referenz_0 = np.loadtxt('spectrometer_0_referenz_at_650.00nm_cut_at_1184.50Y_01.
 wavelength, ref_0 = np.hsplit(referenz_0, 2)
 
 referenz_90 = np.loadtxt('spectrometer_90_referenz_at_650.00nm_cut_at_1184.50Y_01.dat', skiprows = 6)
-wavelength, ref_90 = np.hsplit(referenz_0, 2)
+wavelength, ref_90 = np.hsplit(referenz_90, 2)
 
 # berechne Energie bzw. Winkelfrequenzskala
 energyscale = np.zeros(wavelength.size)
@@ -54,57 +80,75 @@ for i in np.arange(wavelength.size):
     energyscale[i] = h * c / (e * np.array(wavelength[i]) * np.power(10, -9.))
     w[i] = 2 * np.pi * c / (wavelength[i] * 10 ** (-9)) * 10 ** (-15.)
 
+
+
 ########## Auswertung Einzel-Partikel
 ##################################################################
 
 groesse = ['100', '70']
-print(np.arange(len(groesse)))
 
-for i in np.arange(len(groesse)):
+for i in np.arange(1):#len(groesse)):
+    plt.figure(i, dpi= 100)#figsize= [15,10])
+
 
     einzel_0 = np.loadtxt('spectrometer_0_'+groesse[i]+'nm_einzel_at_650.00nm_cut_at_1184.50Y_01.dat', skiprows = 6)
-    wavelength, intensity1 = np.hsplit(einzel_0, 2)
+    wavelength, intensity_0 = np.hsplit(einzel_0, 2)
     einzel_90 = np.loadtxt('spectrometer_90_'+groesse[i]+'nm_einzel_at_650.00nm_cut_at_1184.50Y_01.dat', skiprows = 6)
-    wavelength, intensity2 = np.hsplit(einzel_90, 2)
+    wavelength, intensity_90 = np.hsplit(einzel_90, 2)
 
-    abs1 = 1-intensity1/ref_0
-    abs2 = 1-intensity2/ref_90
-    plt.figure(i)
-    plt.plot(w, abs1, label='0 Grad Polarisation')
-    plt.plot(w, abs2, label='90 Grad Polariation')
+    abs_0 = 1-intensity_0/ref_0
+    abs_90 = 1-intensity_90/ref_90
+
+    plt.plot(w, abs_0, label='0 Grad Polarisation')
+    #plt.plot(w, abs_90, label='90 Grad Polariation')
     plt.xlabel('omega *10^(15)')
     plt.title('Extinktionsspektrum für '+groesse[i]+' nm Nanopartikel')
 
     w = np.squeeze(w)
-    abs1 = np.squeeze(abs1)
-    abs2 = np.squeeze(abs2)
+    abs_0 = np.squeeze(abs_0)
+    abs_90 = np.squeeze(abs_90)
 
-    start = round(w.size*2/5)
+    start = round(w.size/2)
     end = w.size
     range_help = [*range(start,end,1) ]
 
-    lorentz_parameter = np.array([2.8, 0.5, 0.1], dtype=float)
-    gauss_parameter = np.array([3.0, 0.1, 0.2, 0.1], dtype=float)
+    # lorentz_parameter = np.array([2.8, 0.5, 0.1], dtype=float)
+    # gauss_parameter = np.array([3.0, 0.1, 0.2, 0.1], dtype=float)
 
-    fwhm = np.zeros(2)
-
-    #plt.plot(w,lorentz(w,2.9, 0.5))
-    #plt.plot(w,gaussian(w,3.4, 0.3, 0.3))
-    #plt.plot(w,lorentz(w,3.0, 0.4)*gaussian(w,3.3, 0.5, 0.3))
-    parameters, covariance_matrix = curve_fit(faltung, w, abs1, p0=[ 3.0, 0.4, 3.3, 0.5, 0.3 ])
+    ## Lorentz*Gauss Fit
+    parameters, covariance_matrix = curve_fit(faltung, w, abs_0, p0=[ 3.0, 0.4, 3.3, 0.5, 0.3 ])
     w0, gamma, mu, sig, fac = parameters
-    fwhm[0] = np.sqrt( w0**2+gamma*w0 ) - np.sqrt( w0**2-gamma*w0 )
-    plt.plot(w, faltung(w, w0, gamma, mu, sig, fac),
-             label="Fit 0 Grad, FWHM = "+str(round(fwhm[0]*100)/100)+" *10^(15)")
+    func = faltung(w, w0, gamma, mu, sig, fac)
+    fwhm = FWHM(w,func)
+    plt.plot(w, func,
+             label="Lorentz*Gauss-Fit 0 Grad")
+    plt.plot( w[[int(fwhm[1]),int(fwhm[2])]] , func[[int(fwhm[1]),int(fwhm[2])]] ,  lw=1 ,ls='--',
+              label="FWHM = "+str(round(fwhm[0]*100)/100)+" *10^(15)")
+    plt.legend(loc='upper right')
 
-    parameters, covariance_matrix = curve_fit(faltung, w, abs2, p0=[3.0, 0.4, 3.3, 0.5, 0.3])
-    w0, gamma, mu, sig, fac = parameters
-    fwhm[1] = np.sqrt(w0 ** 2 + gamma * w0) - np.sqrt(w0 ** 2 - gamma * w0)
-    plt.plot(w, faltung(w, w0, gamma, mu, sig, fac),
-             label="Fit 0 Grad, FWHM = " + str(round(fwhm[1] * 100) / 100) + " *10^(15)")
+    #parameters, covariance_matrix = curve_fit(faltung, w, abs2, p0=[3.0, 0.4, 3.3, 0.5, 0.3])
+    #w0, gamma, mu, sig, fac = parameters
+    #fwhm[1] = np.sqrt(w0 ** 2 + gamma * w0) - np.sqrt(w0 ** 2 - gamma * w0)
+    #plt.plot(w, faltung(w, w0, gamma, mu, sig, fac),
+    #         label="Fit 0 Grad, FWHM = " + str(round(fwhm[1] * 100) / 100) + " *10^(15)")
 
-    print(fwhm, " in 10^(15)")
-    plt.legend()
+
+    #### Rest der Überlegungen
+    ## Lorentz Fit
+    # parameters, covariance_matrix = curve_fit(lorentz, w[range_help], abs_0[range_help], p0=[3.0, 0.4])
+    # w0, gamma = parameters
+    # fwhm[0] = f_L(w0, gamma)
+    # plt.plot(w, lorentz(w, w0, gamma),
+    #             label="Lorentz-Fit 0 Grad, FWHM = " + str(round(fwhm[0] * 100) / 100) + " *10^(15)")
+
+    ## Gauss Fit
+    # parameters, covariance_matrix = curve_fit(gaussian, w[range_help], abs_0[range_help], p0=[3.3, 0.5, 0.3])
+    # mu, sig, fac = parameters
+    # fwhm[1] = f_G(sig)
+    # plt.plot(w, gaussian(w, mu, sig, fac),
+    #             label="Gauss-Fit 0 Grad, FWHM = " + str(round(fwhm[1] * 100) / 100) + " *10^(15)")
+
+
 
 
 ## Load Hybridisierte
@@ -121,7 +165,7 @@ wavelength, intensity = np.hsplit(hybrid_0_20nm, 2)
 
 
 # nehme einfach mal für intensität beide Polarisationen
-intensity = intensity1+intensity2
+intensity = intensity_0+intensity_90
 
 
 
@@ -156,7 +200,7 @@ abs = np.squeeze(abs)
 #plt.plot(wavelength, gaussian(wavelength, mu, sig, fac), label="Fit Peak 1")
 
 # fit mit d (zus. Verschiebung in y Richtung)
-parameters, covariance_matrix = curve_fit(func1, wavelength, abs1, p0=[650, 50, 0.6, 0.1])
+parameters, covariance_matrix = curve_fit(func1, wavelength, abs_0, p0=[650, 50, 0.6, 0.1])
 mu, sig, fac, d = parameters
 #plt.plot(wavelength, func1(wavelength, mu, sig, fac, d), label="Fit Peak 2")
 
@@ -176,7 +220,7 @@ mu, sig, fac, d = parameters
 #fwhm = 2 * np.sqrt(2*np.log(2)) * sig
 #print(fwhm)
 
-plt.legend()
+#plt.legend()
 plt.show()
 
 
