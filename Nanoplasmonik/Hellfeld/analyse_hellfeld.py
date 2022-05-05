de9a1d7a1f21cc8f52d0621ff73169f326c85e1f
Neo-practise <marius.kaiser.mail@web.de>
2022-05-05 18:08:35 +0200

    Fehler vom fwhm mit berechnet

--- Nanoplasmonik/Hellfeld/analyse_hellfeld.py
+++ Nanoplasmonik/Hellfeld/analyse_hellfeld.py
@@ -34,7 +34,7 @@ def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     return array[idx]
 
-def FWHM( x, y ): # Gibt Float-Array zurück [ fwhm, index links, index rechts ]
+def FWHM( x, y , orig): # Gibt Float-Array zurück [ fwhm, fwhm_fehler, index links, index rechts ]
     ID = np.zeros(2, dtype = int)
     half_values = np.zeros(2, dtype=float)
     ID_max = list(y).index(y.max())
@@ -42,7 +42,24 @@ def FWHM( x, y ): # Gibt Float-Array zurück [ fwhm, index links, index rechts ]
     half_values[1] = find_nearest(y[ID_max + 1:-1], y.max() / 2)
     ID[0] = list(y).index(half_values[0])
     ID[1] = list(y).index(half_values[1])
-    fwhm = np.array( [np.abs(w[ID[0]]-w[ID[1]]), ID[0], ID[1] ], dtype = float )
+    #### berechne Unsicherheit
+    ## berechne jede Abweichung in w-Richtung und Intensitätsrichtung (zweiteres müsste noch)
+    bereich=400     #Bereich um halbes Maximum
+    abweichung_links = np.zeros(2*bereich)
+    abweichung_rechts = np.zeros(2*bereich)
+    abweichung_oben = np.zeros(2 * bereich) ## Abweichung Modell zur Peak-Hoehe
+    j = 0
+    while j < bereich:
+        abweichung_links[bereich+j] = np.abs(y[ID[0]+j]-find_nearest(orig[0:ID_max], y[ID[0]+j]))
+        abweichung_links[bereich - j-1] = np.abs(y[ID[0]-j-1]-find_nearest(orig[ID_max:-1], y[ID[0]-j-1]))
+        abweichung_rechts[bereich+j] = np.abs(y[ID[1]+j]-find_nearest(orig[0:ID_max], y[ID[1]+j]))
+        abweichung_rechts[bereich - j-1] = np.abs(y[ID[1]-j-1]-find_nearest(orig[ID_max:-1], y[ID[1]-j-1]))
+        abweichung_oben[bereich+j] = np.abs(y[ID_max+j]-find_nearest(orig, y[ID_max+j]))
+        abweichung_oben[bereich - j -1] = np.abs(y[ID_max - j-1]-find_nearest(orig, y[ID_max - j-1]))
+        j += 1
+    delta_w = np.sqrt(np.max(abweichung_links)**2 + np.max(abweichung_rechts)**2 + np.max(abweichung_oben)**2)
+    #print(delta_w)
+    fwhm = np.array( [np.abs(x[ID[0]]-x[ID[1]]), delta_w, ID[0], ID[1]  ], dtype = float )
     #print(ID)
     return fwhm
 
@@ -117,23 +134,28 @@ for i in np.arange(len(groesse)):
 
     ## Lorentz*Gauss Fit
     parameters, covariance_matrix = curve_fit(faltung, w, abs_0, p0=[ 3.0, 0.4, 3.3, 0.5, 0.3 ])
+    std_parameters = np.sqrt(np.diag(covariance_matrix))
+    # Gibt laut Python-Documentation die Standardabweichung der Parameter an
+    # Guter Fehler für w ist vlt sqrt( sdt_w0^2 +std_mu^2 )
+    #delta_w = np.sqrt( std_parameters[0]**2. + std_parameters[2]**2. )
+    #print('Aus Parameter: ' , delta_w)
     w0, gamma, mu, sig, fac = parameters
     func = faltung(w, w0, gamma, mu, sig, fac)
-    fwhm = FWHM(w,func)
+    fwhm = FWHM(w,func, abs_0)
     plt.plot(w, func,
              label="Lorentz*Gauss-Fit 0 Grad")
-    plt.plot( w[[int(fwhm[1]),int(fwhm[2])]] , func[[int(fwhm[1]),int(fwhm[2])]] ,  lw=1 ,ls='--',
-              label="FWHM = "+str(round(fwhm[0]*100)/100)+" *10^(15)")
+    plt.plot( w[[int(fwhm[2]),int(fwhm[3])]] , func[[int(fwhm[2]),int(fwhm[3])]] ,  lw=1 ,ls='--',
+              label="FWHM = "+str(round(fwhm[0]*1000)/1000)+"± "+str(round(fwhm[1]*1000)/1000)+" *10^(15)")
     plt.legend(loc='upper right')
 
     parameters, covariance_matrix = curve_fit(faltung, w, abs_90, p0=[2.65, 0.3, 4.2, 0.8, 2.])
     w0, gamma, mu, sig, fac = parameters
     func = faltung(w, w0, gamma, mu, sig, fac)
-    fwhm = FWHM(w,func)
+    fwhm = FWHM(w,func, abs_90)
     plt.plot(w, func,
              label="Lorentz*Gauss-Fit 90 Grad")
-    plt.plot(w[[int(fwhm[1]), int(fwhm[2])]], func[[int(fwhm[1]), int(fwhm[2])]], lw=1, ls='--',
-             label="FWHM = " + str(round(fwhm[0] * 100) / 100) + " *10^(15)")
+    plt.plot(w[[int(fwhm[2]), int(fwhm[3])]], func[[int(fwhm[2]), int(fwhm[3])]], lw=1, ls='--',
+             label="FWHM = " + str(round(fwhm[0] * 1000) / 1000)+"± "+str(round(fwhm[1]*1000)/1000)+" *10^(15)")
     plt.legend(loc='upper right')
 
 
